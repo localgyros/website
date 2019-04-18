5ed753676f5339d0cc7fe1b498ad331e0038ae38
Benjamin H Boruff <ben@boruff.me>
2019-04-18 16:47:44 -0400

    refs #8 fixed siteName bug

--- source/admin.blade.php
+++ source/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Admin | {{ $page->site->title }}</title>
+    <title>Admin | {{ $page->siteName }}</title>
     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </head>
 <body>
